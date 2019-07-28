1868d02c1374ecbacb0c38ccc02c4916917c4f84
Johnonyango <j.yayah7@gmail.com>
2019-07-28 14:07:50 +0300

    add changes and folder structure complete

---
+++ app/models.py
@@ -0,0 +1,27 @@
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,country,language,image):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.image = image
+        self.country = country
+        self.language = language
+
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date
---
+++ start.sh
@@ -0,0 +1,3 @@
+export NEWS_API_KEY='a7e48eef1cb644049222a9b8fde752cf'
+
+python3.6 manage.py server
